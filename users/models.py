79ec5820d8c3fb28bbfde1626727b17811f3f640
jm-jung <jmsk7554@gmail.com>
2024-09-12 19:00:35 +0900

    user model생성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password,phone_number, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
+        if not phone_number:
+            raise ValueError('Users must have an phone number')
+        email=self.normalize_email(email)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password,phone_number, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password,phone_number, **extra_fields)
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    nickname = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=15, null=False, blank=False)
+    phone_number = models.CharField(max_length=100, null=False, blank=False)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.nickname
