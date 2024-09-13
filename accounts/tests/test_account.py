45fd967a475d447063e5184389615dd4b83f580d
moonyu <dbwls3681@naver.com>
2024-09-13 10:11:05 +0900

    test코드 user생성코드 추가

--- accounts/tests/test_account.py
+++ accounts/tests/test_account.py
@@ -2,17 +2,21 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from accounts.models import AccountModel
-from users.models import Users  # Users 모델이 기본 User 모델이라고 가정
+from users.models import Users
 
 
 class AccountsTestCase(TestCase):
 
     def setUp(self) -> None:
 
-        self.user = Users.objects.create(username="테스트유저")
+        self.user = Users.objects.create_user(
+            email="testuser@example.com",
+            password="testpass",
+            phone_number="01012345678"
+        )
 
         self.account = AccountModel.objects.create(
-            user_id=self.user,
+            user=self.user,
             account_number="1234567890",
             bank_code="kakao",
             account_type="savings",
@@ -22,7 +26,7 @@ class AccountsTestCase(TestCase):
     def test_account_creation(self) -> None:
 
         account = AccountModel.objects.get(account_number="1234567890")
-        self.assertEqual(account.user_id, self.user)
+        self.assertEqual(account.user, self.user)
         self.assertEqual(account.bank_code, "kakao")
         self.assertEqual(account.account_type, "savings")
         self.assertEqual(account.balance, 1000.00)
@@ -31,7 +35,7 @@ class AccountsTestCase(TestCase):
 
         with self.assertRaises(IntegrityError):
             AccountModel.objects.create(
-                user_id=self.user,
+                user=self.user,
                 account_number="1234567890",
                 bank_code="kb",
                 account_type="checking",
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django_mini",
-        "USER": "woojinan",
+        "NAME": "mydb",
+        "USER": "mac",
         "PASSWORD": "1234",
         "HOST": "localhost",
         "PORT": "5432",
