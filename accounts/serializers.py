d198a3da55bfd881716c7488f4b1c1d9aa252d9f
moonyu <dbwls3681@naver.com>
2024-09-13 17:25:53 +0900

    account_view

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
+
 from accounts.models import AccountModel
+from transactions.models import Transaction
 
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountModel
-        fields = ['account_number', 'bank_code', 'account_type', 'balance']
-        read_only_fields = ['id', 'balance']
+        fields = '__all__'
+        read_only_fields = ['id','balance']
 
 
 
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        read_only_fields = ['id', 'balance_after_transaction', 'transaction_datetime']
